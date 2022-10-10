0c601dd823067ba5f52d14c25fd4c9c06bf2690d
Fernanda <f_ernanda@outlook.com.br>
2022-10-10 19:09:25 -0300

    Add find by id method and specialized exceptions

--- backend/src/main/java/com/acme/catalog/services/CategoryService.java
+++ backend/src/main/java/com/acme/catalog/services/CategoryService.java
@@ -3,11 +3,13 @@ package com.acme.catalog.services;
 import com.acme.catalog.dto.CategoryDTO;
 import com.acme.catalog.entities.Category;
 import com.acme.catalog.repositories.CategoryRepository;
+import com.acme.catalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,5 +22,12 @@ public class CategoryService {
     public List<CategoryDTO> findAll() {
         List<Category> entities = repository.findAll();
         return entities.stream().map(CategoryDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> optionalEntity = repository.findById(id);
+        Category entity = optionalEntity.orElseThrow(() -> new EntityNotFoundException("Entity not found."));
+        return new CategoryDTO(entity);
     }
 }
