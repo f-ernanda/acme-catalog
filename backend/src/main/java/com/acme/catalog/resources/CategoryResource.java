63bef45c518b3d6eb7b8ca1a89cd9f55be28b8e9
Fernanda <f_ernanda@outlook.com.br>
2022-10-15 10:43:27 -0300

    Add pagination

--- backend/src/main/java/com/acme/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/acme/catalog/resources/CategoryResource.java
@@ -3,12 +3,14 @@ package com.acme.catalog.resources;
 import com.acme.catalog.dto.CategoryDTO;
 import com.acme.catalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -18,8 +20,14 @@ public class CategoryResource {
     CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll() {
-        List<CategoryDTO> categories = service.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "12") Integer size,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO> categories = service.findAllPaged(pageRequest);
         return ResponseEntity.ok(categories);
     }
 
